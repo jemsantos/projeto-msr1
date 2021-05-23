8df949b00e1e3ff5b9d5f133040db8efedea0c6b
José Santos <eustaquio.cadastros@gmail.com>
2021-05-23 14:17:12 -0300

    Final code of class - M-3_Implementando ação não-CRUD

--- src/main/java/com/algaworks/algalog/domain/model/Entrega.java
+++ src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -24,6 +24,7 @@ import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 
 import com.algaworks.algalog.domain.ValidationGroups;
+import com.algaworks.algalog.domain.exception.NegocioException;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
@@ -84,6 +85,30 @@ public class Entrega {
 		this.getOcorrencias().add(ocorrencia);
 		
 		return ocorrencia;
+
+	}
+
+	public void finalizar() {
+
+		//if (!entrega.getStatus().equals(StatusEntrega.PENDENTE)) {
+		if (naoPodeSerFinalizada()) {
+			throw new NegocioException("Entrega não pode ser finalizada");
+		}
+
+		setStatus(StatusEntrega.FINALIZADA);
+		setDataFinalizacao(OffsetDateTime.now());
+
+	}
+	
+	public boolean podeSerFinalizada() {
+
+		return StatusEntrega.PENDENTE.equals(getStatus());
+
+	}
+	
+	public boolean naoPodeSerFinalizada() {
+
+		return !podeSerFinalizada();
 
 	}
 
